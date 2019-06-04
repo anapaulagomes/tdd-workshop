e8428db8b376ca11bc36e1ca13d746cd3ad72a70
Ana Paula Gomes <apgomes88@gmail.com>
2019-06-04 12:58:21 +0200

    Add food and drinks problem [WIP]

---
+++ 3_food_and_drinks/food_and_drinks.py
@@ -0,0 +1,18 @@
+"""
+Let's say your company is hosting the next PyLadies meetup.
+One of your tasks is calculating how much food and drinks
+would be needed to buy.
+
+Somebody told you that you should go for 1 pizza 
+for every 10 people who RSVPed and 2 orange juices
+per person. You already know that 20% of the RSVPs
+usually don't show up. This number should be included
+in your calculations as well if the number of RSVPs is
+greater than 20.
+
+Your method `calculate()` should receive the number of
+RSVPs and calculate food and drinks for the meetup.
+"""
+
+def calculate(persons):
+    return '1 pizza and 2 orange juices.'
---
+++ 3_food_and_drinks/test_food_and_drinks.py
@@ -0,0 +1,7 @@
+from food_and_drinks import calculate
+
+
+def test_calculate_food_and_drinks_for_one_person():
+    expected_food_and_drinks = '1 pizza and 2 orange juices.'
+
+    assert calculate(1) == expected_food_and_drinks
